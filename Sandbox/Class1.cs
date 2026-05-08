efe07063c1fdad25f7f69adbfb64b5b3d52e4f9b
coolfede97 <coolfede97@gmail.com>
2026-05-08 20:17:39 -0300

    c# made as a worker

---
+++ Sandbox/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Sandbox
+{
+    class ComponentA
+    {
+        [UnmanagedCallersOnly(EntryPoint = "SayIt")]
+        public static void SayIt()
+        {
+            Console.WriteLine("Nasheeee");
+        }
+    };
+}
