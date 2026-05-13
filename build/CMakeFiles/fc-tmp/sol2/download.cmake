af325fd85e5b10f20c05a54ee730f12517a7f3fe
Coolfede97 <coolfede97@gmail.com>
2026-05-13 20:17:34 -0300

    I believe sol2 is fetched correctly

---
+++ build/CMakeFiles/fc-tmp/sol2/download.cmake
@@ -0,0 +1,9 @@
+cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
+
+message(VERBOSE "Executing download step for sol2")
+
+block(SCOPE_FOR VARIABLES)
+
+include("/home/coolfede97/coding/sandbox/build/CMakeFiles/fc-tmp/sol2/sol2-gitclone.cmake")
+
+endblock()
