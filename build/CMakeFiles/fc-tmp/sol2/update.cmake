70471db487c96fbf6cbc3429fe78dcabb4a1bccc
Federico Ugarte <111787840+CoolFede97@users.noreply.github.com>
2026-05-13 20:19:32 -0300

    Delete build directory

--- build/CMakeFiles/fc-tmp/sol2/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing update step for sol2")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/home/coolfede97/coding/sandbox/build/CMakeFiles/fc-tmp/sol2/sol2-gitupdate.cmake")
-
-endblock()
